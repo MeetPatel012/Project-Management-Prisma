260b4e530929158aad4a5bf12f67d23657bae7a2
Meet-Patel-123 <companywork012@gmail.com>
2025-04-16 11:50:25 +0530

    error fix of deploymet resuablepriority

--- src/app/priority/reusablePriorityPage/page.tsx
+++ src/app/priority/reusablePriorityPage/page.tsx
@@ -3,11 +3,15 @@ import { getTasksByUserAndPriority } from "@/server-actions/_priority_actions";
 import ReusablePriorityPageComponent from "./PriorityPageComponent";
 
 interface PageProps {
-  priority: Priority;
+  params: {};
+  searchParams: {
+    priority?: Priority;
+  };
 }
 
-export default async function PriorityPage({ priority }: PageProps) {
+export default async function PriorityPage({ searchParams }: PageProps) {
   const userId = 1; // You might want to get this from session/auth
+  const priority = searchParams.priority as Priority;
   const tasks = await getTasksByUserAndPriority(userId, priority);
   return (
     <ReusablePriorityPageComponent initialTasks={tasks} priority={priority} />
